383dd9af16563ce8711e88f945c89e672af64a87
solsry <jks96399@naver.com>
2023-05-31 10:08:38 +0900

    My first modify test :)

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -2,6 +2,7 @@ package ch01;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hi ! i am Solsry");
+        System.out.println("Hi ! i am Solsry :)");
+        System.out.println("Modify test :)");
     }
 }
